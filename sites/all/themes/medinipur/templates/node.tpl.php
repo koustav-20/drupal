6dcec153f74433f4296d2b044c9e3cfe3cd12622
abusalam <abusalamparvezalam@gmail.com>
2015-08-01 20:06:06 +0530

    Some Linting

--- sites/all/themes/medinipur/templates/node.tpl.php
+++ sites/all/themes/medinipur/templates/node.tpl.php
@@ -82,34 +82,35 @@
 ?>
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
-  <?php print $user_picture; ?>
+    <?php print $user_picture; ?>
 
-  <?php print render($title_prefix); ?>
-  <?php if (!$page): ?>
-    <h4<?php print $title_attributes; ?> class='no-margin-top'><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h4>
-  <?php endif; ?>
-  <?php print render($title_suffix); ?>
+    <?php print render($title_prefix); ?>
+    <?php if (!$page): ?>
+        <h4<?php print $title_attributes; ?> class='no-margin-top'><a
+                href="<?php print $node_url; ?>"><?php print $title; ?></a></h4>
+    <?php endif; ?>
+    <?php print render($title_suffix); ?>
 
-  <?php if ($display_submitted): ?>
-    <div class="submitted">
-      <?php print $submitted; ?>
+    <?php if ($display_submitted): ?>
+        <div class="submitted">
+            <?php print $submitted; ?>
+        </div>
+    <?php endif; ?>
+
+    <div class="content"<?php print $content_attributes; ?>>
+        <?php
+        // We hide the comments and links now so that we can render them later.
+        hide($content['comments']);
+        hide($content['links']);
+        print render($content);
+        ?>
     </div>
-  <?php endif; ?>
 
-  <div class="content"<?php print $content_attributes; ?>>
-    <?php
-      // We hide the comments and links now so that we can render them later.
-      hide($content['comments']);
-      hide($content['links']);
-      print render($content);
-    ?>
-  </div>
+    <?php print render($content['links']); ?>
 
-  <?php print render($content['links']); ?>
-
-  <?php print render($content['comments']); ?>
+    <?php print render($content['comments']); ?>
 
 </div>
 <?php if (!$page): ?>
-<hr class="dotted">
- <?php endif; ?>
+    <hr class="dotted">
+<?php endif; ?>
--- sites/all/themes/medinipur/templates/views-view-fields--block.tpl.php
+++ sites/all/themes/medinipur/templates/views-view-fields--block.tpl.php
@@ -25,37 +25,39 @@
  */
 $role = '';
 ?>
-<?php foreach ($fields as $id => $field): 
+<?php foreach ($fields as $id => $field):
 
-if($field->label == 'Block'){
-  $role = strip_tags($field->content);
-}
+    if ($field->label == 'Block') {
+        $role = strip_tags($field->content);
+    }
 
-?>
-  <?php if (!empty($field->separator)): ?>
+    ?>
+    <?php if (!empty($field->separator)): ?>
     <?php print $field->separator; ?>
-  <?php endif; ?>
+<?php endif; ?>
 
-  <?php print $field->wrapper_prefix; ?>
+    <?php print $field->wrapper_prefix; ?>
     <?php print $field->label_html; ?>
     <?php print $field->content; ?>
-  <?php print $field->wrapper_suffix; ?>
+    <?php print $field->wrapper_suffix; ?>
 <?php endforeach; ?>
 
 <?php
 $taxonomy_id = taxonomy_get_term_by_name($role);
 $tid = 0;
 foreach ($taxonomy_id as $taxonomy_item) {
-        $tid = $taxonomy_item->tid;
-      }
-	  
+    $tid = $taxonomy_item->tid;
+}
+
 $parent = taxonomy_get_parents($tid);
 
-if(isset($parent) && !empty($parent)){
-	$immediate_head = $parent[key($parent)]->name;
-}else{
-	$immediate_head = 'NA';
+if (isset($parent) && !empty($parent)) {
+    $immediate_head = $parent[key($parent)]->name;
+} else {
+    $immediate_head = 'NA';
 }
 ?>
 <br/>
-<span class="views-field views-field-field-contact-number">    <span class="views-label views-label-field-contact-number">Immediate Head: </span>    <span class="field-content"><?php print $immediate_head;?></span>  </span>
+<span class="views-field views-field-field-contact-number">    <span
+        class="views-label views-label-field-contact-number">Immediate Head: </span>    <span
+        class="field-content"><?php print $immediate_head; ?></span>  </span>
