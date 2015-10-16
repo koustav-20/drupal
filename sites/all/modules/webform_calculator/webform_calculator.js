f912f7642abfb0075a8dfd9a21147078a57fe01b
abusalam <abusalamparvezalam@gmail.com>
2015-10-16 13:24:04 +0530

    Project webform_calculator (7.x-2.0-alpha3) Update

--- sites/all/modules/webform_calculator/webform_calculator.js
+++ sites/all/modules/webform_calculator/webform_calculator.js
@@ -9,9 +9,27 @@
         var elements = Drupal.webformCalculator.getComponentsKeys(component);
 
         $(elements).each(function(index, componentKey) {
-          $('input[name$="[' + componentKey + ']"]', context).unbind('keyup').bind('keyup', function(event) {
+          var handler = function (event) {
             Drupal.webformCalculator.evaluateAllFormulas();
-          });
+          };
+          var selector = ''
+                  + 'input:text[name$="[' + componentKey + ']"]' // Number, Single select
+                  + ', '
+                  + 'select[name$="[' + componentKey + ']"]' // Number, Single select
+                  + ', '
+                  + 'select[name$="[' + componentKey + '][]"]'// Multiple select
+                  + ', '
+                  + 'input:radio[name$="[' + componentKey + ']"]'// Radios
+                  + ', '
+                  + '#edit-submitted-' + componentKey + ' input:checkbox' // Checkboxes
+                  ;
+
+
+          $(selector, context)
+              .unbind('change', handler).bind('change', handler) // Something changed
+              .unbind('keyup', handler).bind('keyup', handler) // Even before we leave input element
+              .unbind('mouseup', handler).bind('mouseup', handler) // Also care for paste context menu
+          ;
         });
       }
     }
@@ -26,13 +44,44 @@
 
   // Replace tokens from formula with numeric values from components.
   Drupal.webformCalculator.evaluateFormula = function(formulaComponent) {
-    console.log(formulaComponent);
     var formulaReplaced = formulaComponent.value;
     var elements = Drupal.webformCalculator.getComponentsKeys(formulaComponent);
 
     var invalidFields = [];
     $(elements).each(function(index, componentKey) {
-      var componentValue = $('input[name$="[' + componentKey + ']"]').val();
+      var componentValue;
+      // Number
+      componentValue = componentValue ||
+        $('input:text[name$="[' + componentKey + ']"]').val();
+      // Single select
+      componentValue = componentValue ||
+        $('select[name$="[' + componentKey + ']"]').val();
+      // Multiple select (provides array if exists)
+      componentValue = componentValue ||
+        $('select[name$="[' + componentKey + '][]"]').val();
+      // Radios and checkboxes (provides array but only if exists)
+      var checkables = $('#edit-submitted-' + componentKey + ' input:checkbox'
+      + ', '
+      + 'input:radio[name$="[' + componentKey + ']"]'
+      );
+      if (checkables.length > 0) {
+        componentValue = componentValue ||
+        checkables.map(
+            function() {return this.checked ? this.value : undefined;}
+        ).get();
+      }
+      // Formula
+      componentValue = componentValue ||
+        $('#formula-component-' + componentKey).text();
+
+      // Care for array
+      if (componentValue && componentValue instanceof Array) {
+        // Convert to number if possible
+        componentValue = componentValue.map(function (a) {return parseFloat(a) || a;});
+        // Summarize
+        componentValue = componentValue.length ? componentValue.reduce(function (a, b) {return a + b;}) : 0;
+      }
+
       if (isNaN(componentValue) || componentValue == '') {
         var label =  $('label[for$=-' + componentKey.replace('_', '-') + ']').text().trim();
         if (label == '') {
@@ -45,15 +94,23 @@
       }
     });
 
+    var formulaComponentElement = $('#formula-component-' + formulaComponent.form_key);
+
     if (invalidFields.length > 0) {
       invalidFields = Drupal.webformCalculator.unique(invalidFields);
-      var message = Drupal.t('Enter correct value for %fields to see result.', {'%fields': invalidFields.join(', ')});
-      $('#formula-component-' + formulaComponent.form_key).html(message);
+      // Set message.
+      var message = formulaComponent.extra.error_message || Drupal.t('Enter correct value for %fields to see result.', {'%fields': invalidFields.join(', ')});
+      formulaComponentElement.html(message);
+      // Hide prefix and suffix.
+      formulaComponentElement.parent().find('.field-prefix, .field-suffix').hide();
     }
     else {
+      // Set result.
       var formulaResult = eval(formulaReplaced);
       formulaResult = Drupal.webformCalculator.round(formulaResult, formulaComponent.extra.precision);
-      $('#formula-component-' + formulaComponent.form_key).text(formulaResult);
+      formulaComponentElement.text(formulaResult);
+      // Show prefix and suffix.
+      formulaComponentElement.parent().find('.field-prefix, .field-suffix').show();
     }
   };
 
