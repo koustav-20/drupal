cef4cac473a3d44cee3df2b0d1798a9a88b742e1
Abu Salam (Latitude E5450) <abusalamparvezalam@gmail.com>
2016-02-21 09:13:27 +0530

    Fixed: Bootstrap Affix Nav Causes Div Below To Jump Up

--- sites/all/themes/district_portal/templates/page.tpl.php
+++ sites/all/themes/district_portal/templates/page.tpl.php
@@ -119,52 +119,54 @@
   </div>
 </div>
 
-<header id="navbar" role="banner" class="<?php print $navbar_classes; ?>"
-  data-spy="affix" data-offset-top="170">
-  <div class="container-fluid">
-    <div class="navbar-header visible-xs">
-      <?php if ($logo): ?>
-        <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>"
-           title="<?php print t('Home'); ?>">
-          <img src="<?php print $logo; ?>" width="19px"
-               alt="<?php print t('Home'); ?>"/>
-        </a>
-      <?php endif; ?>
+<div class="navbar-wrapper">
+  <header id="navbar" role="banner" class="<?php print $navbar_classes; ?>"
+          data-spy="affix" data-offset-top="170">
+    <div class="container-fluid">
+      <div class="navbar-header visible-xs">
+        <?php if ($logo): ?>
+          <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>"
+             title="<?php print t('Home'); ?>">
+            <img src="<?php print $logo; ?>" width="19px"
+                 alt="<?php print t('Home'); ?>"/>
+          </a>
+        <?php endif; ?>
 
-      <?php if (!empty($site_name)): ?>
-        <a class="name navbar-brand" href="<?php print $front_page; ?>"
-           title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
-      <?php endif; ?>
+        <?php if (!empty($site_name)): ?>
+          <a class="name navbar-brand" href="<?php print $front_page; ?>"
+             title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
+        <?php endif; ?>
 
-      <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
-      <button type="button" class="navbar-toggle" data-toggle="collapse"
-              data-target=".navbar-collapse">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-    </div>
+        <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+        <button type="button" class="navbar-toggle" data-toggle="collapse"
+                data-target=".navbar-collapse">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+      </div>
 
-    <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
-      <div class="navbar-collapse collapse">
-        <nav role="navigation">
-          <?php if (!empty($primary_nav)): ?>
-            <?php print render($primary_nav); ?>
-          <?php endif; ?>
-          <span class="visible-xs collapse">
+      <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
+        <div class="navbar-collapse collapse">
+          <nav role="navigation">
+            <?php if (!empty($primary_nav)): ?>
+              <?php print render($primary_nav); ?>
+            <?php endif; ?>
+            <span class="visible-xs collapse">
             <?php if (!empty($secondary_nav)): ?>
               <?php print render($secondary_nav); ?>
             <?php endif; ?>
           </span>
-          <?php if (!empty($page['navigation'])): ?>
-            <?php print render($page['navigation']); ?>
-          <?php endif; ?>
-        </nav>
-      </div>
-    <?php endif; ?>
-  </div>
-</header>
+            <?php if (!empty($page['navigation'])): ?>
+              <?php print render($page['navigation']); ?>
+            <?php endif; ?>
+          </nav>
+        </div>
+      <?php endif; ?>
+    </div>
+  </header>
+</div>
 
 <div class="main-container container-fluid">
 
