1b262d6c8a53ea9caff8cb6e37f228effdae6d30
Abu Salam (Latitude E5450) <abusalamparvezalam@gmail.com>
2016-03-26 11:58:33 +0530

    Custom maintanance and offline page

--- sites/all/themes/district_portal/template.php
+++ sites/all/themes/district_portal/template.php
@@ -4,3 +4,22 @@
  * @file
  * template.php
  */
+
+/**
+ * Implements hook_preprocess_maintenance_page().
+ */
+function district_portal_preprocess_maintenance_page(&$variables) {
+  // By default, site_name is set to Drupal if no db connection is available
+  // or during site installation. Setting site_name to an empty string makes
+  // the site and update pages look cleaner.
+  // @see template_preprocess_maintenance_page
+  if (isset($variables['db_is_active']) && !$variables['db_is_active']) {
+  // Template suggestion for offline site
+    $variables['theme_hook_suggestion'] = 'maintenance_page__offline';
+  }
+  else {
+  // Template suggestion for live site (in maintenance mode)
+    $variables['theme_hook_suggestion'] = 'maintenance_page';
+  }
+  drupal_add_css(drupal_get_path('theme', 'bartik') . '/css/maintenance-page.css');
+}
