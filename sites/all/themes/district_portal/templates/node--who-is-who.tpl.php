b351f831bf9be4020fc0ba2017f7ee36d589654b
Abu Salam (Latitude E5450) <abusalamparvezalam@gmail.com>
2016-04-21 09:59:30 +0530

    Updated Who-is-who template

---
+++ sites/all/themes/district_portal/templates/node--who-is-who.tpl.php
@@ -0,0 +1,11 @@
+<div class="card-portrait">
+  <div class="thumbnail">
+    <img class="img-responsive"
+      src="<?php print file_create_url($node->field_photograph[LANGUAGE_NONE][0]['uri']); ?>"
+      width="100%"/>
+    <div class="caption">
+      <strong class="text-primary"><?php print $content['field_post']['0']['#title']; ?></strong>
+      <p><?php print $content['field_full_name']['#items']['0']['value']; ?></p>
+    </div>
+  </div>
+</div>
