e666504884afbbd2df3b42b4fea5226540c8aa62
Abu Salam (Latitude E5450) <abusalamparvezalam@gmail.com>
2016-05-03 16:44:43 +0530

    Splash of Colors with Footer Fix

--- sites/all/themes/district_portal/templates/page.tpl.php
+++ sites/all/themes/district_portal/templates/page.tpl.php
@@ -80,145 +80,150 @@
 $mobile_nav = $secondary_nav;
 
 ?>
-  <div class="<?php print $container_class; ?> visible-sm visible-md visible-lg"
-       id="site-header" role="banner">
-    <?php if (!empty($secondary_nav) || !empty($page['top_nav'])): ?>
-      <div class="pull-right">
-        <nav role="navigation">
-          <?php if (!empty($secondary_nav)): ?>
-            <?php print render($secondary_nav); ?>
-          <?php endif; ?>
-          <?php if (!empty($page['top_nav'])): ?>
-            <?php print render($page['top_nav']); ?>
-          <?php endif; ?>
-        </nav>
-      </div>
-    <?php endif; ?>
-
-    <?php if ($logo): ?>
-      <a class="logo-lg pull-left" href="<?php print $front_page; ?>"
-         title="<?php print t('Home'); ?>">
-        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>"/>
-      </a>
-    <?php endif; ?>
-    <?php if (!empty($site_name)): ?>
-      <div class="site-name">
-
-        <a class="inset-text-effect" href="<?php print $front_page; ?>"
-           title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
-
-
-        <?php if (!empty($site_slogan)): ?>
-          <p class="lead"><?php print $site_slogan; ?></p>
+<div class="<?php print $container_class; ?> visible-sm visible-md visible-lg"
+     id="site-header" role="banner">
+  <?php if (!empty($secondary_nav) || !empty($page['top_nav'])): ?>
+    <div class="pull-right">
+      <nav role="navigation">
+        <?php if (!empty($secondary_nav)): ?>
+          <?php print render($secondary_nav); ?>
         <?php endif; ?>
+        <?php if (!empty($page['top_nav'])): ?>
+          <?php print render($page['top_nav']); ?>
+        <?php endif; ?>
+      </nav>
+    </div>
+  <?php endif; ?>
 
-      </div>
-    <?php endif; ?>
-  </div>
-  <header id="navbar" role="banner" class="<?php print $navbar_classes; ?>">
-    <div class="<?php print $container_class; ?>">
-      <div class="navbar-header visible-xs">
+  <?php if (!empty($site_name)): ?>
+    <div class="site-name">
+
+      <p>
         <?php if ($logo): ?>
-          <a class="logo navbar-btn pull-left"
-             href="<?php print $front_page; ?>"
+          <a class="logo-lg pull-left" href="<?php print $front_page; ?>"
              title="<?php print t('Home'); ?>">
-            <img src="<?php print $logo; ?>" width="19px"
-                 alt="<?php print t('Home'); ?>"/>
+            <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>"/>
           </a>
         <?php endif; ?>
 
-        <?php if (!empty($site_name)): ?>
-          <a class="name navbar-brand" href="<?php print $front_page; ?>"
-             title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
-        <?php endif; ?>
+        <a class="inset-text-effect" href="<?php print $front_page; ?>"
+           title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
+      </p>
 
-        <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
-        <button type="button" class="navbar-toggle" data-toggle="collapse"
-                data-target=".navbar-collapse">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-      </div>
+      <?php if (!empty($site_slogan)): ?>
+        <p class="lead"><?php print $site_slogan; ?></p>
+      <?php endif; ?>
 
-      <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
-        <div class="navbar-collapse collapse">
-          <nav role="navigation">
-            <?php if (!empty($primary_nav)): ?>
-              <?php print render($primary_nav); ?>
-            <?php endif; ?>
-            <span class="visible-xs collapse">
+    </div>
+  <?php endif; ?>
+</div>
+
+<header id="navbar" role="banner" class="<?php print $navbar_classes; ?>">
+  <div class="<?php print $container_class; ?>">
+    <div class="navbar-header visible-xs">
+      <?php if ($logo): ?>
+        <a class="logo navbar-btn pull-left"
+           href="<?php print $front_page; ?>"
+           title="<?php print t('Home'); ?>">
+          <img src="<?php print $logo; ?>" width="19px"
+               alt="<?php print t('Home'); ?>"/>
+        </a>
+      <?php endif; ?>
+
+      <?php if (!empty($site_name)): ?>
+        <a class="name navbar-brand" href="<?php print $front_page; ?>"
+           title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
+      <?php endif; ?>
+
+      <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+      <button type="button" class="navbar-toggle" data-toggle="collapse"
+              data-target=".navbar-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
+
+    <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
+      <div class="navbar-collapse collapse">
+        <nav role="navigation">
+          <?php if (!empty($primary_nav)): ?>
+            <?php print render($primary_nav); ?>
+          <?php endif; ?>
+          <span class="visible-xs collapse">
               <?php if (!empty($secondary_nav)): ?>
                 <?php print render($mobile_nav); ?>
               <?php endif; ?>
             </span>
-            <?php if (!empty($page['navigation'])): ?>
-              <?php print render($page['navigation']); ?>
-            <?php endif; ?>
-          </nav>
-        </div>
-      <?php endif; ?>
-    </div>
-  </header>
-
-  <div class="main-container  <?php print $container_class; ?>">
-
-    <header role="banner" id="page-header">
-      <?php print render($page['header']); ?>
-    </header>
-    <!-- /#page-header -->
-
-    <div class="row">
-
-      <?php if (!empty($page['sidebar_first'])): ?>
-        <aside class="col-sm-3" role="complementary">
-          <?php print render($page['sidebar_first']); ?>
-        </aside>  <!-- /#sidebar-first -->
-      <?php endif; ?>
-
-      <section<?php print $content_column_class; ?>>
-        <?php if (!empty($page['highlighted'])): ?>
-          <div
-            class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
-        <?php endif; ?>
-        <?php if (!empty($breadcrumb)): print $breadcrumb; endif; ?>
-        <a id="main-content"></a>
-        <?php print render($title_prefix); ?>
-        <?php if (!empty($title)): ?>
-          <h1 class="page-header"><?php print $title; ?></h1>
-        <?php endif; ?>
-        <?php print render($title_suffix); ?>
-        <?php print $messages; ?>
-        <?php if (!empty($tabs)): ?>
-          <?php print render($tabs); ?>
-        <?php endif; ?>
-        <?php if (!empty($page['help'])): ?>
-          <?php print render($page['help']); ?>
-        <?php endif; ?>
-        <?php if (!empty($action_links)): ?>
-          <ul class="action-links"><?php print render($action_links); ?></ul>
-        <?php endif; ?>
-        <?php print render($page['content']); ?>
-      </section>
-
-      <?php if (!empty($page['sidebar_second'])): ?>
-        <aside class="col-sm-3" role="complementary">
-          <?php print render($page['sidebar_second']); ?>
-        </aside>  <!-- /#sidebar-second -->
-      <?php endif; ?>
-
-    </div>
+          <?php if (!empty($page['navigation'])): ?>
+            <?php print render($page['navigation']); ?>
+          <?php endif; ?>
+        </nav>
+      </div>
+    <?php endif; ?>
   </div>
-<?php if (!empty($page['footer'])): ?>
-  <footer class="footer <?php print $container_class; ?>">
-    <div id="grass"></div>
+</header>
+
+<div class="main-container  <?php print $container_class; ?>">
+
+  <header role="banner" id="page-header">
+    <?php print render($page['header']); ?>
+  </header>
+  <!-- /#page-header -->
+
+  <div class="row">
+
+    <?php if (!empty($page['sidebar_first'])): ?>
+      <aside class="col-sm-3" role="complementary">
+        <?php print render($page['sidebar_first']); ?>
+      </aside>  <!-- /#sidebar-first -->
+    <?php endif; ?>
+
+    <section<?php print $content_column_class; ?>>
+      <?php if (!empty($page['highlighted'])): ?>
+        <div
+          class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
+      <?php endif; ?>
+      <?php if (!empty($breadcrumb)): print $breadcrumb; endif; ?>
+      <a id="main-content"></a>
+      <?php print render($title_prefix); ?>
+      <?php if (!empty($title)): ?>
+        <h1 class="page-header"><?php print $title; ?></h1>
+      <?php endif; ?>
+      <?php print render($title_suffix); ?>
+      <?php print $messages; ?>
+      <?php if (!empty($tabs)): ?>
+        <?php print render($tabs); ?>
+      <?php endif; ?>
+      <?php if (!empty($page['help'])): ?>
+        <?php print render($page['help']); ?>
+      <?php endif; ?>
+      <?php if (!empty($action_links)): ?>
+        <ul class="action-links"><?php print render($action_links); ?></ul>
+      <?php endif; ?>
+      <?php print render($page['content']); ?>
+    </section>
+
+    <?php if (!empty($page['sidebar_second'])): ?>
+      <aside class="col-sm-3" role="complementary">
+        <?php print render($page['sidebar_second']); ?>
+      </aside>  <!-- /#sidebar-second -->
+    <?php endif; ?>
+
+  </div>
+</div>
+
+<footer class="footer <?php print $container_class; ?>">
+  <div id="grass">
     <div id="niclogo"></div>
-    <div id="footer-links" class="center-block">
+  </div>
+  <?php if (!empty($page['footer'])): ?>
+    <div id="footer-links">
       <?php print render($page['footer']); ?>
     </div>
-    <div id="copyright" class="<?php print $container_class; ?>">
-      <span>Copyright &copy; 2015 Paschim Medinipur</span>
-    </div>
-  </footer>
-<?php endif; ?>
+  <?php endif; ?>
+  <div id="copyright" class="<?php print $container_class; ?>">
+    <span>Copyright &copy; 2015 Paschim Medinipur</span>
+  </div>
+</footer>
