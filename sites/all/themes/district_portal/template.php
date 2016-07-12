ed267b9d22c4531594bcbd3e5af67f27b33f7df8
Abu Salam (Latitude E5450) <abusalamparvezalam@gmail.com>
2016-07-12 18:41:57 +0530

    Display Webform Image using theme
    Ref :: drupal.org/node/1413562

--- sites/all/themes/district_portal/template.php
+++ sites/all/themes/district_portal/template.php
@@ -23,3 +23,35 @@ function district_portal_preprocess_maintenance_page(&$variables) {
   }
   //drupal_add_css(drupal_get_path('theme', 'bartik') . '/css/maintenance-page.css');
 }
+
+/**
+ * Implements hook_webform_display_file().
+ */
+
+function district_portal_webform_display_file($variables) {
+  $element = $variables['element'];
+
+  $file = $element['#value'];
+  $url = !empty($file) ? webform_file_url($file->uri) : t('no upload');
+
+  if( !empty($file) ) {
+    $image_extensions = array("png", "bmp", "jpg", "jpeg", "gif");
+    $extension = explode(".", $file->uri);
+    $extension = $extension[count($extension) - 1];
+
+    $is_image = in_array($extension, $image_extensions);
+
+    if( $element['#format'] == 'text' ){
+      return $url;
+    } else if( !$is_image ){
+      return l($file->filename, $url);
+    } else if( $is_image ){
+      $output = '<a href="' . webform_file_url($file->uri) . '">';
+      $output .= '<img class="img-responsive" src="' . webform_file_url($file->uri) . '"/> </a>';
+             return $output;
+         }
+     }
+
+     return ' ';
+ }
+
